cd6a5eb8a698cb8e8939ca1ea6a8a4bad3fdcd6f
Sachin Bhatt <bhattsachin@gmail.com>
2012-10-13 16:01:00 -0400

    client

---
+++ src/edu/buffalo/util/Client.java
@@ -0,0 +1,50 @@
+package edu.buffalo.util;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Client {
+	private Socket socket;
+	private String host; //because we can connect to any host
+	
+	public Client(String host){
+		this.host = host;
+	}
+	
+	public void send(String text, int port){
+		String response = null;
+		try{
+			
+			socket = new Socket(host, port);
+			socket.setSoTimeout(500);
+			ObjectOutputStream os = new ObjectOutputStream(
+					socket.getOutputStream());
+			os.writeObject(text);
+			
+			ObjectInputStream inpstr = new ObjectInputStream(
+					socket.getInputStream());
+			response = (String) inpstr.readObject();
+			
+		}catch (ClassNotFoundException cx) {
+			cx.printStackTrace();
+			// why this kolaveri?
+			
+
+		} catch (InterruptedIOException timeout) {
+			timeout.printStackTrace();
+			
+		} catch (IOException ex) {
+			// there can be so many reasons why this error coming up.
+			// let treat this as timeout
+			ex.printStackTrace();
+			
+		} finally {
+			System.out.println("RESPONSE:" + response);
+			
+		}
+	}
+
+}
